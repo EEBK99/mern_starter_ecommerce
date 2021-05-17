c8c4d961fd85f7cff7f27af76147538b80e761cc
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-05-17 22:05:10 +0500

    updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,12 +26,37 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  res.json({
-    msg: "User Loggedin",
-  });
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(401).json({
+        error: "Invalid Email! ",
+      });
+    }
+
+    console.log(user);
+
+    if (password == user.password) {
+      res.json({
+        msg: "Password Matched!",
+      });
+    } else {
+      res.json({ msg: "Invalid Password!" });
+    }
+
+    res.json({
+      msg: "User Loggedin",
+    });
+  } catch (error) {
+    console.log("Something Unexpected Happened!", error.message);
+    return res.json({
+      error: "Server Error!",
+    });
+  }
 });
 
 module.exports = router;
